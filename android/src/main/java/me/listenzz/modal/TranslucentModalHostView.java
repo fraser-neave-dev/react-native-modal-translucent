388faa5aaeb70d01652d79b7b11a5bbe10e791d7
yinwen <wendy.yinwen@fn.com.my>
2019-06-18 14:30:40 +0800

    update to androidx

--- android/src/main/java/me/listenzz/modal/TranslucentModalHostView.java
+++ android/src/main/java/me/listenzz/modal/TranslucentModalHostView.java
@@ -6,7 +6,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
-import androidx.core.v4.view.ViewCompat;
+import androidx.core.view.ViewCompat;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowInsets;
